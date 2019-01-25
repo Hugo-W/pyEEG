4d849b524e1a3fad1a62438b60a515ad8c712d0a
Hugo-W <hugo.weissbart@gmail.com>
2019-01-25 19:14:54 +0000

    added file (empty) structure

---
+++ pyeeg/utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Name of module
+~~~~~~~~~~~~~~~
+
+Desctription.
+
+Usage:
+       
+Author: Hugo Weissbart
+Date:
+"""
+
+#### Libraries
+# Standard library
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+# My libraries
+
+
+def main(input_txt):
+    # some code for main() function
+
+if __name__ == "__main__":
+    # Here some code for main behaviour
